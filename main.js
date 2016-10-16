031a4ed5c68b57b77fb6a1bf13e523cfb43cbd34
pnpo <nuno43549@gmail.com>
2016-10-16 23:45:10 +0100

    windows information

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+const app_dir = 'file://' + __dirname + '/app';
+
 const electron = require('electron');
 const{app, BrowserWindow} = electron;
 
+//windows
+const win = null;
+const senson = null;
 
 app.on('ready', ()=>{
-    var mainWindow = new BrowserWindow({width:800, height:600});
+    var win = new BrowserWindow({width:800, height:600});
     //mainWindow();
 });
