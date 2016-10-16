d9b79f67867ba04dcdbb09c52298cca4421bcadc
pnpo <nuno43549@gmail.com>
2016-10-17 00:18:52 +0100

    Added main model for Exam; changes in main window

---
+++ app/js/examination-app.js
@@ -0,0 +1,38 @@
+function Student() {
+    this.name = '';
+    this.number = -1;
+}
+
+function Answer() {
+    this.number = -1;
+    this.text = '';
+}
+
+function Question() {
+    this.number = -1;
+    this.text = '';
+    this.alineas = null //array of Question;
+}
+
+
+function Exam() {
+    this.student = new Student();
+    this.id = '';
+    this.questions = null; //array of questions;
+    this.answers = null; //array of answers;
+    this.url = '';
+}
+
+Exam.prototype.load = function () {
+
+}
+
+Exam.prototype.render = function() {
+
+}
+
+Exam.prototype.formatPDF = function() {
+
+}
+
+exports.Exam = new Exam();
