285585ed993f94c80212d4fba336f5802b975ea2
pnpo <nuno43549@gmail.com>
2016-11-06 22:44:29 +0000

    whatchdog barks whenever it becomes alive

--- app/js/watchdog.js
+++ app/js/watchdog.js
@@ -1,19 +1,24 @@
 const {ipcRenderer} = require('electron');
 
-sniff();
-
 let main_notified = false;
 
+bark();
+sniff();
+
+
+
 function sniff() {
-   var timer = setInterval(function(){
-        if(navigator.onLine && !main_notified){
-            //alert('online');
-            main_notified = true;
-            ipcRenderer.send('internet-connected',null);
-        }
-        else if(! navigator.onLine){
-            main_notified = false;
-            ipcRenderer.send('internet-disconnected', null);
-        }
-    }, 3000);
+    //bark();
+   var timer = setInterval(bark, 3000);
+}
+
+function bark() {
+    if(navigator.onLine && !main_notified){
+        main_notified = true;
+        ipcRenderer.send('internet-connected',null);
+    }
+    else if(! navigator.onLine){
+        main_notified = false;
+        ipcRenderer.send('internet-disconnected', null);
+    }
 }
